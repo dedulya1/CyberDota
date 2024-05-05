e906a5b4bdc775909ed7ed27948743d550c0d5ca
Ivan <ivanmatsur1@gmail.com>
2024-05-06 00:35:59 +0300

    sonar update 2

--- src/test/java/com/project/opendota/service/MatchServiceTest.java
+++ src/test/java/com/project/opendota/service/MatchServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
